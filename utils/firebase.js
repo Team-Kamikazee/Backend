666438b06041445e792215a7000f080b1f37e496
Devabubakar <abubakarali3w@gmail.com>
2022-11-21 10:08:27 +0300

    add: added useFetch and firebase utils

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,7 +1,87 @@
-// setup firebase authentication
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import { createContext, useContext, useState, useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBV7jGWmSqWIDPldlzz6293FQTF47_2W2g',
+  authDomain: 'netflix-clone-45ce0.firebaseapp.com',
+  projectId: 'netflix-clone-45ce0',
+  storageBucket: 'netflix-clone-45ce0.appspot.com',
+  messagingSenderId: '332588188509',
+  appId: '1:332588188509:web:55a228fd4259225c605eb4',
+  measurementId: 'G-2Y8BM440YC',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 
-// setup firebase instance
+export const AuthContext = createContext<GlobalType | null>(null);
+
+//provide children with access to context
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+//wrapper provider component
+const AuthProvider = ({ children }) => {
+  const auth = useProvideAuth();
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
+
+//hook to create auth object handle states
+export const useProvideAuth = () => {
+  const [user, setUser] = useState<object | null | boolean>(null);
+  const [loading, setLoading] = useState(true);
+  const [email, setEmail] = useState('');
+
+  const signIn = (email,password) => {
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        setUser(response.user);
+        setLoading(false);
+
+        return response.user;
+      });
+  };
+
+  const signUp = (email, password) => {
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+        setUser(response.user);
+        return response.user;
+      });
+  };
+
+  const signOut = () => {
+    return firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setUser(null);
+        setLoading(false);
+      });
+  };
+
+  //component mounts
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        setLoading(false);
+      } else {
+        setUser(false);
+        setLoading(false);
+      }
+    });
+    //clean up subscription
+    return () => unsubscribe();
+  }, [user]);
+
+  return { user, signIn, signOut, loading, signUp, email, setEmail };
+};
---
+++ utils/protectedRoutes.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+import { useState, useEffect } from 'react';
+
+function useFetch() {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState<unknown | null>(null);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [url]);
+
+  return { data, isLoading, error };
+}
+
+export default useFetch;
